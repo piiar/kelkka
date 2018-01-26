0172705d2023c54ddb2419be75150f1b525fd337
Markku Painomaa <markku.painomaa@octo3.fi>
2018-01-27 00:59:23 +0200

    mouse controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour {
 
     private string userId = null;
     private Vector3 movement = Vector3.zero;
+    private float lookAngle = 0f;
     private CharacterController controller;
 
 	// Use this for initialization
@@ -15,26 +16,22 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
 
-        float mouseX = Input.GetAxis("Mouse X"); // + Input.GetAxis("Horizontal") * 10f;
-        float mouseY = Input.GetAxis("Mouse Y"); // + Input.GetAxis("Vertical") * -8f;
+        Vector3 direction = new Vector3(h, 0f, v);
+        //if(direction != Vector3.zero) {
+        //    direction.y = 0f;
+        //    Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
+        //}
 
-        Vector3 direction = new Vector3(h, transform.position.y, v);
-        if(direction != Vector3.zero) {
-            direction.y = 0f;
-            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
-        }
+        lookAngle += mouseX * 2f;
+        transform.rotation = Quaternion.Euler(0f, lookAngle, 0f);
+
         movement = direction * Time.deltaTime * 5f;
     }
 
     void FixedUpdate() {
         controller.Move(movement);
     }
-
-
-    //public void HandleRotationMovement(Vector3 input) {
-    //    float lookAngle += input.x * 2f;
-    //    transform.rotation = Quaternion.Euler(0f, lookAngle, 0f);
-    //}
 }
