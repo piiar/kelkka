705fc117f47803100316ee24fec90089585375c6
Sampsoni <sami.repo@live.com>
2018-01-27 00:58:59 +0200

    Watcher Robot Ai

---
+++ Assets/Scripts/WatcherRobot/WatcherRobotMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WatcherRobotMovement : MonoBehaviour {
+    public GameObject target;
+
+    public void NavigateToTarget(){
+        GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            NavigateToTarget();
+        }
+    }
+}
