4741fd617272140de34c989c4b0eaa43b804f9e1
Sampsoni <sami.repo@live.com>
2018-01-26 23:08:59 +0200

    PlayerList and PlayerListItem Scripts.

---
+++ Assets/Scripts/Lobby/PlayerList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerList : MonoBehaviour {
+
+    public PlayerListItem playerListItemPrefab;
+    private List<string> players;
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < 20; i++)
+        {
+            AddPlayer("sami" + i);
+        }
+
+	}
+
+    public void AddPlayer(string playerName){
+        PlayerListItem nameLabel = Instantiate(playerListItemPrefab) as PlayerListItem;
+        nameLabel.gameObject.SetActive(true);
+        nameLabel.transform.SetParent(this.transform, false);
+        nameLabel.SetName(playerName);
+    }
+
+}
---
+++ Assets/Scripts/Lobby/PlayerListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerListItem : MonoBehaviour {
+    public TextMeshProUGUI label; 
+
+    public void SetName(string name){
+        label.SetText(name);
+    }
+
+    public void OnClick(){
+        
+    }
+}
