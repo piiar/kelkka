ef5969db447fe7dd1ccbf7af4cbbbaa37dcf9384
Ioni <sami.repo@live.com>
2018-01-28 00:43:13 +0200

    NamePlate

---
+++ Assets/Scripts/NamePlateRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NamePlateRotation : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Transform lookAtPos = Camera.main.transform;
+        transform.LookAt(2 * transform.position - lookAtPos.position);
+    }
+}
