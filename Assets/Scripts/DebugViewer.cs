26907ea5fde784de88e1214c4c8fcbe087d2ae3e
Vazde <vazde with a domain of dea dot fi>
2018-01-27 19:37:09 +0200

    Added a debugging tool. Usage is simple: add the Script to a GameObject in the scene, and call DebugViewer.Debug(key, value)

---
+++ Assets/Scripts/DebugViewer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugViewer : MonoBehaviour {
+    private static Dictionary<string, string> data = new Dictionary<string, string>();
+
+    public static void Debug(string key, object value) {
+        data[key] = value.ToString();
+    }
+
+    void OnGUI() {
+        int y = 10;
+        foreach (var kvp in data) {
+            string s = kvp.Key + ": " + kvp.Value;
+            GUI.Label(new Rect(10, y, 1000, 20), s);
+            y += 20;
+        }
+    }
+}
